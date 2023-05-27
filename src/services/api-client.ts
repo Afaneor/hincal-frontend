80d97fe7e54c2e37dcc52cfa46a89151549d12da
Igor Sokolov <is@letsnova.ru>
2023-05-27 14:29:25 +0300

    fix: путь до апишки

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -7,7 +7,7 @@ const paramsSerializer = (params: any) => {
 }
 
 const config = {
-  baseURL: 'http://localhost:8000/api',
+  baseURL: '/api',
   timeout: 30000,
   withCredentials: true,
   paramsSerializer,
