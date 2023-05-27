3bdfda8c6abc7064772de866055e3997a081141f
Nikolay Pavlin <afaneor@gmail.com>
2023-05-27 16:27:25 +0300

    fix: css load

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import 'antd/dist/reset.css'
 import '../styles/global.css'
+import '../../public/antd.min.css'
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import type { AppProps } from 'next/app'
