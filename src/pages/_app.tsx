c5617de01da7dd422e652b118ebcfaac7e6be91a
Igor Sokolov <is@letsnova.ru>
2023-05-27 15:00:14 +0300

    feat: задержка рендера, чтобы css успел подгрузиться

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,6 +8,8 @@ import React from 'react'
 
 import NotificationMessageProvider from '@/components/NotificationMessage/NotificationMessage'
 
+import RenderDelay from '../components/RenderDelay/RenderDelay'
+
 const qClientConfig = {
   defaultOptions: {},
 }
@@ -15,11 +17,13 @@ const qClientConfig = {
 const queryClient = new QueryClient(qClientConfig)
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
-    <QueryClientProvider client={queryClient}>
-      <NotificationMessageProvider>
-        <Component {...pageProps} />
-      </NotificationMessageProvider>
-    </QueryClientProvider>
+    <RenderDelay>
+      <QueryClientProvider client={queryClient}>
+        <NotificationMessageProvider>
+          <Component {...pageProps} />
+        </NotificationMessageProvider>
+      </QueryClientProvider>
+    </RenderDelay>
   )
 }
 
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -7,7 +7,7 @@ const paramsSerializer = (params: any) => {
 }
 
 const config = {
-  baseURL: '/api',
+  baseURL: 'http://localhost:8000/api',
   timeout: 30000,
   withCredentials: true,
   paramsSerializer,
